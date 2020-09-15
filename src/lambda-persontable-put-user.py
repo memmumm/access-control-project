7e45f89da8e270d4b9f78c6dde5fc694a56f50e0
MitjaHaimila <mitja.haimila@gmail.com>
2020-09-15 09:41:14 +0300

    put user lisätty

--- src/lambda-persontable-put-user.py
+++ src/lambda-persontable-put-user.py
@@ -4,20 +4,21 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('person')
 
-def create_new_user(event, context):
 
-    jsondata = json.loads(event)
+def create_new_user(event, context):
+    message = event['Records'][0]['Sns']['Message']
+    userdata = json.loads(message)
+
     item = {
-        'id': str(jsondata['userId']),
-        'firstname': jsondata['firstName'],
-        'lastname': jsondata['lastName'],
+        'id': userdata['userId'],
+        'firstname': userdata['firstName'],
+        'lastname': userdata['lastName']
     }
     table.put_item(Item=item)
 
     return {
-        "id": jsondata['userId']
+        "created": json.dumps(item)
     }
-
 
 
 
